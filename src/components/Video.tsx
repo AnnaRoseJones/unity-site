7aad67c7b6e4f2abd47537ee3a4f8b4851c7a1c7
AnnaRose Jones <annarosejones@AnnaRoses-MacBook-Pro.local>
2024-03-27 15:16:26 -0500

    absolute path homepage background video

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,11 +1,11 @@
-import unityvid from "../assets/video/unitysite.mp4";
+//import unityvid from "/videos/unitysite.mp4";
 import React from "react";
 
 const VideoBackground = () => {
   return (
     <div className="video-container">
-      <video className="video-background" autoPlay loop muted>
-        <source src={unityvid} type="video/mp4" />
+      <video className="video-background" autoPlay loop muted playsInline>
+        <source src={"/videos/unitysite.mp4"} type="video/mp4" />
       </video>
       {/* Add other elements on top of the video as needed */}
     </div>
